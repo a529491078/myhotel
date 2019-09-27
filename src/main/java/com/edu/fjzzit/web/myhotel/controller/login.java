0962473929954b5b1e12b6c1508c736b7f58b9e9
zlyCodeKing <2444639551@qq.com>
2019-09-27 18:15:44 +0800

    使用Swagger，实现测试和文档查看z

--- src/main/java/com/edu/fjzzit/web/myhotel/controller/login.java
+++ src/main/java/com/edu/fjzzit/web/myhotel/controller/login.java
@@ -2,15 +2,25 @@ package com.edu.fjzzit.web.myhotel.controller;
 
 import com.edu.fjzzit.web.myhotel.config.ResultJson;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@Api(tags = "用户管理")
 public class login {
 
-    @RequestMapping("/login")
-    public ResultJson login(String name,String password){
+    @PostMapping("/login")
+    @ApiOperation("用户登录")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "userName",value = "用户名",dataType = "string",required = true),
+            @ApiImplicitParam(name = "password",value = "密码",dataType = "string",required = true)
+    })
+    public ResultJson login(String userName,String password){
         return new ResultJson("200","登录成功",null);
     }
 }
