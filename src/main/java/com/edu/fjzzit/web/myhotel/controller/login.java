0c88b4dfe70ae230559f908f6fd35eb1f5ea5d66
zlyCodeKing <2444639551@qq.com>
2019-09-27 18:09:35 +0800

    简单login(测试)接口

--- src/main/java/com/edu/fjzzit/web/myhotel/controller/login.java
+++ src/main/java/com/edu/fjzzit/web/myhotel/controller/login.java
@@ -1,8 +1,15 @@
 package com.edu.fjzzit.web.myhotel.controller;
 
 import com.edu.fjzzit.web.myhotel.config.ResultJson;
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/user")
 public class login {
+
+    @RequestMapping("/login")
     public ResultJson login(String name,String password){
         return new ResultJson("200","登录成功",null);
     }
